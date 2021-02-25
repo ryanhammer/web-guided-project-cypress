ac45f1d5ec11596b0cbc871813155e2f18a55f6b
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-25 10:37:55 -0700

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,11 @@
 // write tests here
+describe('Quotes app', () => {
+    // Each test needs clean state
+    // Test cant rely on previous tests
+    // Every test needs to work in isolation
+    // before each test navigate to http://localhost:1234
+    beforeEach(() => {
+        cy.visit('http://localhost:1234')
+    })
+
+})
