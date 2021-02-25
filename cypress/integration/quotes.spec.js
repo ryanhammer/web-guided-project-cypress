de2f9e4560f35a65e1b697f3a95ce7e868b7baa2
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-25 11:53:55 -0700

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -75,7 +75,7 @@ describe('Quotes app', () => {
             textInput().type('Pizza is the best!!')
             authorInput().type('Tony Stark')
             submitButton().click()
-            cy.contains('Pizza is the best!!')
+            cy.contains('Pizza is the best!!').siblings('button:nth-of-type(2)').click()
         })
 
 
